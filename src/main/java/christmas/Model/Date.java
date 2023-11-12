ab203d0eb61e34aaa82fd8eab739b31efcf58238
ekeprl <ykeon26@naver.com>
2023-11-12 22:18:28 +0900

    날짜 입력 및 validate

---
+++ src/main/java/christmas/Model/Date.java
@@ -0,0 +1,32 @@
+package christmas.Model;
+
+import christmas.Util.Rule;
+
+public class Date {
+    public final int date;
+
+    public Date(int date) {
+        validate(date);
+        this.date = date;
+    }
+    private void validate(int inputValue) {
+        validateRange(inputValue);
+        validateZero(inputValue);
+    }
+
+    private void validateRange(int inputValue) {
+        if (inputValue > Rule.MAX_DATE) {
+            throw new IllegalArgumentException(Rule.DATERANGE_ERROR);
+        }
+    }
+
+    private void validateZero(int inputValue) {
+        if(inputValue == Rule.DATEZERO) {
+            throw new IllegalArgumentException(Rule.DATEZERO_PATTERN);
+        }
+    }
+
+    public int getDate(){
+        return date;
+    }
+}
