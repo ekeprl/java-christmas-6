d994e780ed180672ea18f748ddde1d4b4f775dc8
ekeprl <ykeon26@naver.com>
2023-11-12 17:52:47 +0900

    컨트롤러의 run 실행하도록 try-cach문을 이용해 작성.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,15 @@
 package christmas;
 
+import christmas.Controller.ChristmasController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            ChristmasController christmasController = new ChristmasController();
+            christmasController.run();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
