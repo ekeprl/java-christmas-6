9cc3068d6a1808b0043f40960dcd593167ccc7a3
mulgom <mulgom.cresoty@gmail.com>
2023-11-13 17:47:54 +0900

    getMenu 추가 - order에서 받은것들을 메뉴 / 가격으로 나눠서 menuList로 리턴

--- src/main/java/christmas/Controller/ChristmasController.java
+++ src/main/java/christmas/Controller/ChristmasController.java
@@ -1,9 +1,19 @@
 package christmas.Controller;
 
 import christmas.Model.Date;
+import christmas.Model.Menu;
+import christmas.Model.Order;
+import christmas.Model.Sale;
 import christmas.View.InputDateView;
+import christmas.View.InputMenuView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static christmas.View.OuputSaleView.printSale;
 import static christmas.View.OutputDateView.printDate;
+import static christmas.View.OutputMenuView.printMenu;
 
 public class ChristmasController {
 
@@ -11,8 +21,11 @@ public class ChristmasController {
         Date date = getDate();
         printDate(date);
 
+        List<Order> menu = getMenu();
+        printMenu(menu);
 
-
+        Sale sale = getSale();
+        printSale(sale);
 
 
     }
@@ -21,6 +34,22 @@ public class ChristmasController {
         InputDateView inputDateView = new InputDateView();
         int date = inputDateView.getValue();
         return new Date(date);
+    }
+
+    private List<Order> getMenu() {
+        InputMenuView inputMenuView = new InputMenuView();
+        List<String> inputValue = inputMenuView.getValue();
+        ArrayList<Order> menuList = new ArrayList();
+        for(String menuData : inputValue) {
+            Menu menu = Menu.getMenu(menuData.split("-")[0]);
+            Order order = new Order(menu, Integer.parseInt(menuData.split("-")[1]));
+            menuList.add(order);
+        }
+        return menuList;
+    }
+
+    private Sale getSale(List<Menu> Menus) {
+        totalSale = Sale.get
 
     }
 }
