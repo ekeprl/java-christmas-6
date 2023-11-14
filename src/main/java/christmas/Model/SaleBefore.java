ec6712852d7180b7e0c39da4998f13a1739e6ffe
mulgom <mulgom.cresoty@gmail.com>
2023-11-14 17:44:51 +0900

    할인 적용이 안될때의 총 주문가격 확인하는 클래스로 변경.

---
+++ src/main/java/christmas/Model/SaleBefore.java
@@ -0,0 +1,13 @@
+package christmas.Model;
+
+
+public class SaleBefore {
+
+    final int nosale ;
+
+
+    public SaleBefore(int nosale, Order order) {
+        if(order.getMenu() )
+        this.nosale = nosale;
+    }
+}
