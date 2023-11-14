e0bf08fd9ed19fc7fad8fce0d2910fb0cd0c3d7b
mulgom <mulgom.cresoty@gmail.com>
2023-11-14 17:43:36 +0900

    menu, count getter 추가.

--- src/main/java/christmas/Model/Order.java
+++ src/main/java/christmas/Model/Order.java
@@ -9,8 +9,15 @@ public class Order {
     private Menu menu;
     private int count;
 
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
     public Order(Menu menu, int count) {
-        //validate(menus);
         this.menu = menu;
         this.count = count;
     }
