7c770ea27cd93504dd7c8b9749bc63d7c2cdcc86
mulgom <mulgom.cresoty@gmail.com>
2023-11-14 09:39:23 +0900

    주말/평일 구분 수정

--- src/main/java/christmas/Util/Weekdays.java
+++ src/main/java/christmas/Util/Weekdays.java
@@ -5,6 +5,7 @@ import christmas.View.InputDateView;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
 public class Weekdays {
@@ -12,9 +13,14 @@ public class Weekdays {
     private final Month MONTH = Month.DECEMBER;
 
     public boolean isWeekday(int day) {
-        LocalDate date = LocalDate.of(YEAR, MONTH, day);
+        String dayStr = String.format("%02d", day);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd");
+        LocalDate date = LocalDate.parse(YEAR + "-" + MONTH.getValue() + "-" + dayStr, formatter);
+
         DayOfWeek dayOfWeek = date.getDayOfWeek();
+
         return dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY;
     }
-
 }
+
