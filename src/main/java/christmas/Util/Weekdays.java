14f32e21eff9140f959645e0be507ed56b650e54
mulgom <mulgom.cresoty@gmail.com>
2023-11-14 17:42:33 +0900

    싱글톤패턴을 적용.
    날짜 입력 시 주말/평일 구분하여 리턴하도록 수정.

--- src/main/java/christmas/Util/Weekdays.java
+++ src/main/java/christmas/Util/Weekdays.java
@@ -6,21 +6,33 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 
 public class Weekdays {
     private final int YEAR = 2023;
     private final Month MONTH = Month.DECEMBER;
 
+    public static Weekdays instance;
+    public static Weekdays getInstance(){
+        if(instance == null){
+            instance = new Weekdays();
+        }
+
+        return instance;
+    }
+
     public boolean isWeekday(int day) {
         String dayStr = String.format("%02d", day);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd");
-        LocalDate date = LocalDate.parse(YEAR + "-" + MONTH.getValue() + "-" + dayStr, formatter);
+        LocalDate date = LocalDate.parse(YEAR + "-" + MONTH + "-" + dayStr, formatter);
 
         DayOfWeek dayOfWeek = date.getDayOfWeek();
 
-        return dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY;
+        if (dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
 
